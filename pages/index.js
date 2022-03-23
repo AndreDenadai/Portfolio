7b05ce5be22508203b00b1ecc9d1f7b9190f9ef8
AndreDenadai <andrefilho77@gmail.com>
2022-03-22 22:52:35 -0300

    atualizando

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,13 @@
-import { useState } from 'react';
+import Link from 'next/link';
 
-function Home() {
-    return <div>
-        <h3>Olá Mundo! // Hello World!!!!</h3>
-        <Contador />
-    </div>
+function Home(){
+    return (<div>
+        <h2>Home</h2>
+
+        
+        <Link href="/sobre">
+            <a>Acessar Página Sobre</a>
+        </Link>
+    </div>)
 }
-
-function Contador(){
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
-
 export default Home
